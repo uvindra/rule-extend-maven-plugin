c51a500b825eb471daa2e0cf0e6702f42cf3a24e
uvindra <uvindra_dj@yahoo.com>
2014-02-23 01:53:07 +0530

    Updated exception handling

--- src/main/java/com/wso2/build/utils/Utility.java
+++ src/main/java/com/wso2/build/utils/Utility.java
@@ -1,17 +1,16 @@
 package com.wso2.build.utils;
 
-import org.apache.commons.io.FileUtils;
 import org.apache.maven.model.Model;
 import org.apache.maven.model.Plugin;
 import org.apache.maven.project.MavenProject;
 import org.codehaus.plexus.util.xml.Xpp3Dom;
 import org.w3c.dom.Document;
-import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
-
+import org.xml.sax.SAXException;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
 import java.io.IOException;
 import java.util.LinkedList;
@@ -92,7 +91,11 @@ public class Utility {
 
             return (0 < nodeList.getLength());
 
-        } catch (Exception e) {
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        } catch (SAXException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
             e.printStackTrace();
         }
 
@@ -125,7 +128,11 @@ public class Utility {
                     }
                 }
             }
-        } catch (Exception e) {
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        } catch (SAXException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
             e.printStackTrace();
         }
 
@@ -174,9 +181,14 @@ public class Utility {
                 }
 
             }
-        } catch (Exception e) {
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        } catch (SAXException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
             e.printStackTrace();
         }
+
 
         return true;
     }
