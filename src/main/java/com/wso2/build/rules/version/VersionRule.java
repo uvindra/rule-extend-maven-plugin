49b249c602dd5d1bf6cdf6068a5cca113e3cebdb
uvindra <uvindra_dj@yahoo.com>
2014-02-24 11:12:01 +0530

    Cleaned up unwanted code

--- src/main/java/com/wso2/build/rules/version/VersionRule.java
+++ src/main/java/com/wso2/build/rules/version/VersionRule.java
@@ -2,6 +2,8 @@ package com.wso2.build.rules.version;
 
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
 import java.util.StringTokenizer;
 
